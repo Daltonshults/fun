d120beb9b3c9610d58701ece08a0fa0bf88d2a70
Daltonshults <Dshults@cocc.edu>
2025-11-03 14:53:29 -0800

    Finished implementation.

--- change.py
+++ change.py
@@ -1,19 +1,34 @@
 class Change:
 
     def __init__(self, total, provided):
-
         self.total = total
         self.provided = provided
-        self.denominations = [100, 50, 20, 10, 5, 2, 1, .25, .1, .05, .01]
+        self.denominations = [100, 50, 20, 10, 5, 1, .25, .1, .05, .01]
+        self.change = self.calculate()
 
 
     def calculate(self):
-        for i in range(len(self.denominations)):
-            change = 0
-            print(self.denominations[i])
         
+        # Calculate total change and initialize list for pair values
+        change_total = self.provided - self.total
+        quantity_by_denomination = []
+
+        # Iterate over the denominations and append pair values to list
+        for i in range(len(self.denominations)):
+
+            # floor divide to get values
+            quantity = change_total // self.denominations[i]
+
+            # Adjust current change total
+            change_total -= self.denominations[i] * quantity
+
+            # If our quantity is greater than 0 add it to the quantity list by denomination
+            if quantity > 0:
+                quantity_by_denomination.append((self.denominations[i], quantity))
+
+            # if our current change total is 0 we no longer need to iterate
+            if change_total == 0:
+                break
 
 
-change = Change(15.00, 17.00)
-
-change.calculate()
+        return quantity_by_denomination
