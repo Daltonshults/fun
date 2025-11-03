226f1173594c5b5ab8ddb0bf22a36bbd714f4331
Daltonshults <Dshults@cocc.edu>
2025-11-03 15:11:59 -0800

    Removed decimals from tests because quantity should never be a float.

--- tests/changetest.py
+++ tests/changetest.py
@@ -6,15 +6,15 @@ class TestChange(unittest.TestCase):
 
     def test_1(self):
         ch = Change(15.00, 17.00)
-        self.assertEqual([(1, 2.00)], ch.change)
+        self.assertEqual([(1, 2)], ch.change)
 
     def test_2(self):
         ch = Change(167.73, 175.00)
-        self.assertEqual([(5, 1.00), (1, 2.00), (0.25, 1.00), (.01, 2.00)], ch.change)
+        self.assertEqual([(5, 1), (1, 2), (0.25, 1), (.01, 2)], ch.change)
 
     def test_3(self):
-        ch = Change(100.00, 275.75)
-        self.assertEqual([(100, 1.00), (50, 1.00), (20, 1.00), (5, 1.00), (0.25, 3.00)], ch.change)
+        ch = Change(100, 275.75)
+        self.assertEqual([(100, 1), (50, 1), (20, 1), (5, 1), (0.25, 3)], ch.change)
         
     def test_negative_change(self):
         ch = Change(175.00, 100.00)
