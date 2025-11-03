e28041cced2d913bdd55e24f06e1d7e4b751e73a
Daltonshults <Dshults@cocc.edu>
2025-11-03 15:02:47 -0800

    Adjusted implementation to handle negative change values.

--- change.py
+++ change.py
@@ -12,23 +12,24 @@ class Change:
         # Calculate total change and initialize list for pair values
         change_total = self.provided - self.total
         quantity_by_denomination = []
+        
+        if change_total > 0:
+            # Iterate over the denominations and append pair values to list
+            for i in range(len(self.denominations)):
 
-        # Iterate over the denominations and append pair values to list
-        for i in range(len(self.denominations)):
+                # floor divide to get values
+                quantity = change_total // self.denominations[i]
 
-            # floor divide to get values
-            quantity = change_total // self.denominations[i]
+                # Adjust current change total
+                change_total -= self.denominations[i] * quantity
 
-            # Adjust current change total
-            change_total -= self.denominations[i] * quantity
+                # If our quantity is greater than 0 add it to the quantity list by denomination
+                if quantity > 0:
+                    quantity_by_denomination.append((self.denominations[i], quantity))
 
-            # If our quantity is greater than 0 add it to the quantity list by denomination
-            if quantity > 0:
-                quantity_by_denomination.append((self.denominations[i], quantity))
-
-            # if our current change total is 0 we no longer need to iterate
-            if change_total == 0:
-                break
+                # if our current change total is 0 we no longer need to iterate
+                if change_total == 0:
+                    break
 
 
         return quantity_by_denomination
